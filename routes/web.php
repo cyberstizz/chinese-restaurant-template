d06a906d8e8655518c5b2e96327814a8225b2531
Charles Lamb <charleslamb@charless-mbp.mynetworksettings.com>
2024-12-24 06:30:42 -0500

    added the routes for the restaruant

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/menu', function () {
+    return view('menu');
+})->name('menu');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
